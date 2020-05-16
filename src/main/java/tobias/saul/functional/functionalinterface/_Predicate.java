eb44d3254479df863a094c78f8c061b927680ccc
Saul Tobias <saul.tobias@fdmgroup.com>
2020-05-16 17:00:06 -0500

    finished predicate

--- src/main/java/tobias/saul/functional/functionalinterface/_Predicate.java
+++ src/main/java/tobias/saul/functional/functionalinterface/_Predicate.java
@@ -17,12 +17,15 @@ public class _Predicate {
 		System.out.println(isPhoneNumberValidPredicate.test("0700000"));
 		System.out.println(isPhoneNumberValidPredicate.test("07000000000"));
 		
-		
-		
+		System.out.println(isPhoneNumberValidPredicate.and(doesPhoneNumberContainNumber3).test("07000000000"));
+		System.out.println(isPhoneNumberValidPredicate.and(doesPhoneNumberContainNumber3).test("07000000003"));
 	}
 	
 	static Predicate<String> isPhoneNumberValidPredicate = phoneNumber -> 
-							phoneNumber.startsWith("07") && phoneNumber.length() ==11;
+							phoneNumber.startsWith("07") && phoneNumber.length() == 11;
+							
+	static Predicate<String> doesPhoneNumberContainNumber3 = phoneNumber ->
+							phoneNumber.contains("3");
  	
 	static boolean isPhoneNumberValid(String phoneNumber) {
 		return phoneNumber.startsWith("07") && phoneNumber.length() ==11;
